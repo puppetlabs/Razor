7b4a2556ba7bc737e8b57849294fb6e2e27c65d5
Daniel Gollub <d.gollub@telekom.de>
2013-09-07 08:05:46 +0200

    Initial DTAG specific preseed for service VMs

---
+++ lib/project_razor/model/dtagcloud_ubuntu_vm.rb
@@ -0,0 +1,39 @@
+require "erb"
+
+# Root ProjectRazor namespace
+module ProjectRazor
+  module ModelTemplate
+    # Root Model object
+    # @abstract
+    class DTAGCloudUbuntuCompute < Ubuntu
+      include(ProjectRazor::Logging)
+
+      def initialize(hash)
+        super(hash)
+        # Static config
+        @hidden = false
+        @name = "dtagcloud_ubuntu_vm"
+        @description = "DTAG Cloud VM"
+        # Metadata vars
+        @hostname_prefix = nil
+        # State / must have a starting state
+        @current_state = :init
+        # Image UUID
+        @image_uuid = true
+        # Image prefix we can attach
+        @image_prefix = "os"
+        # Enable agent brokers for this model
+        @broker_plugin = :agent
+        @osversion = 'precise'
+        @final_state = :os_complete
+        from_hash(hash) unless hash == nil
+      end
+
+      def generate_preseed(policy_uuid)
+        filepath = template_filepath('dtagcloud_ubuntu_vm')
+        ERB.new(File.read(filepath)).result(binding)
+      end
+
+    end
+  end
+end
