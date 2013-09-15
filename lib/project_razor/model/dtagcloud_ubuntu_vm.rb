7a4a217a6f50a0b83c4d1012efea712653e11ddc
Daniel Gollub <d.gollub@telekom.de>
2013-09-15 22:05:28 +0200

    Use custom os_boot scripts and kernel_args for dtagcloud vm model

--- lib/project_razor/model/dtagcloud_ubuntu_vm.rb
+++ lib/project_razor/model/dtagcloud_ubuntu_vm.rb
@@ -29,10 +29,53 @@ module ProjectRazor
         from_hash(hash) unless hash == nil
       end
 
+      def os_boot_script(policy_uuid)
+        @result = "Replied with os boot script"
+        filepath = template_filepath('os_boot_vm_bootstrap')
+        ERB.new(File.read(filepath)).result(binding)
+      end
+
+      def postinstall_call
+        @arg = @args_array.shift
+        case @arg
+          when "inject"
+            fsm_action(:postinstall_inject, :postinstall)
+            return os_boot_script(@policy_uuid)
+          when "boot"
+            fsm_action(:os_boot, :postinstall)
+            return os_complete_script(@node)
+          when "final"
+            fsm_action(:os_final, :postinstall)
+            return ""
+          when "source_fix"
+            fsm_action(:source_fix, :postinstall)
+            return
+          when "send_ips"
+            #fsm_action(:source_fix, :postinstall)
+            # Grab IP string
+            @ip_string = @args_array.shift
+            logger.debug "Node IP String: #{@ip_string}"
+            @node_ip = @ip_string if @ip_string =~ /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/
+            return
+          else
+            fsm_action(@arg.to_sym, :postinstall)
+            return
+        end
+      end
+
+
+
       def generate_preseed(policy_uuid)
         filepath = template_filepath('dtagcloud_ubuntu_vm')
         ERB.new(File.read(filepath)).result(binding)
       end
+
+      def kernel_args(policy_uuid)
+        filepath = template_filepath('dtagcloud_kernel_args_vm')
+        ERB.new(File.read(filepath)).result(binding)
+      end
+
+
 
     end
   end
